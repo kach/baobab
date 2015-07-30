2874e39ce906b0639d42a3f7065cab9788224032
Hardmath123 <hardmath123@gmail.com>
2015-07-29 19:43:51 -0700

    clicking works now

--- baobab.js
+++ baobab.js
@@ -1,6 +1,10 @@
 ~function() {
     function printScene(scene) {
+        var active = true;
+
         var container = document.getElementById("main");
+        var div  = document.createElement("div");
+
         var text = document.createElement("p");
         text.textContent = scene.description;
         var list = document.createElement("ul");
@@ -11,12 +15,22 @@
             var a  = document.createElement("a");
             a.href = "#";
             a.textContent = link.text;
+
+            a.addEventListener("click", function(moi) {
+                if (active) {
+                    active = false;
+                    div.className += " inactive";
+                    a.className += " chosen";
+                }
+            }, false);
+
             li.appendChild(a);
             links.push(a);
             list.appendChild(li);
         });
-        container.appendChild(text);
-        container.appendChild(list);
+        div.appendChild(text);
+        div.appendChild(list);
+        container.appendChild(div);
     }
 
     var test = {"name":"Homewards.","version":"baobab-0.0.1","scenes":{"home":{"description":"You are at home, sweet home.","title":"Your humble abode.","links":[{"text":"Walk the dinosaur.","condition":0,"actions":0},{"text":"Set fire to the rain.","condition":0,"actions":0}]}},"author":"Art Vandelay","description":"A test story.","start":"home","registers":{"health":10,"owns-dinosaur?":true,"is-raining?":false}}
